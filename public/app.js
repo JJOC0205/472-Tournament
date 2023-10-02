6208433dc903e67c7341b0215471677916b32602
Jon O'Connell <jjoc0205@gmail.com>
2023-10-02 19:21:21 -0400

    connected to database, currently  poll

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-/* // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,11 +7,12 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyCYB8LA1-Dfy4A2hQiwjQQrDpf6LaW3tKo",
   authDomain: "project1-17e05.firebaseapp.com",
+  databaseURL: "https://project1-17e05-default-rtdb.firebaseio.com",
   projectId: "project1-17e05",
   storageBucket: "project1-17e05.appspot.com",
   messagingSenderId: "559204208779",
   appId: "1:559204208779:web:c9786385eef630caecc48e"
 };
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig); */
-console.log(firebaseConfig);
+const app = initializeApp(firebaseConfig);
