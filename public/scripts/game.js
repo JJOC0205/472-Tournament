47a1f990f7198922c418cd7049dd87c5f835e3db
Jon O'Connell <jjoc0205@gmail.com>
2023-10-15 14:48:25 -0400

    adding player to screen

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,6 +1,17 @@
-db = firebase.getDatabase();
+/* document.querySelector("#createGame").addEventListener("click", function (event) {
+    var name = document.getElementById("#bracketName").value;
 
-document.querySelector("#createGame").addEventListener("click", function(event) {
-    var name = document.getElementById("#name").value;
-    
+}); */
+document.querySelector("#joinGame").addEventListener("click", function (event) {
+    if (document.getElementById("playerName").value == "") {
+        alert("Please enter a name");
+        return;
+    }
+    let newPlayer = document.createElement("div");
+    newPlayer.id = document.getElementById("playerName").value;
+    newPlayer.innerHTML = document.getElementById("playerName").value;
+    newPlayer.addEventListener("click", function (event) {
+        newPlayer.remove();
+    });
+    document.getElementById("content").appendChild(newPlayer);
 });
