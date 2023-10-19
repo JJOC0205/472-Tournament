cf3d7bfb15677518997910d1d2d29a040d347173
Jon O'Connell <jjoc0205@gmail.com>
2023-10-19 08:23:09 -0400

    current semi working iteration

--- public/index.js
+++ public/index.js
@@ -24,7 +24,7 @@ function renderHome() {
 			return;
 		}
 		let newBracket = db.ref("brackets/").push();
-		newBracket.set({ "name": bracketName, "id": newBracket.key, "players": [{"user": theUser.displayName}] });
+		newBracket.set({ "name": bracketName, "id": newBracket.key, "players": [] });
 	});
 	firebase.database().ref("brackets/").on("value", (snapshot) => {
 		$("#brackets").html("");
@@ -43,32 +43,76 @@ function renderHome() {
 
 function renderBracket(bracketId) {
 	$('#content').html('loading...');
-	db.ref('brackets/').child(bracketId).on('value',(snapshot) => {
-		theBracket = snapshot.val();
+	console.log(bracketId);
+	db.ref("/brackets/").on("value", (snapshot) => {
+		let theBracket = snapshot.val();
+		
 		console.log("HERE");
-		let name = theBracket.name;
-		console.log(name);
-		console.log(theBracket);
+		theBracket = JSON.parse(JSON.stringify(theBracket[bracketId]));
+		console.log(theBracket["players"]);
 		$('#content').html(`
-			<h1>${bracketName}</h1>
+			<h1>${theBracket.name}</h1>
 			<div id="bracket"></div>
 			<button id="addPlayer">Add Player</button>
 			<button id="startGame">Start Game</button>
 		`);
 		let joined = false;
-		theBracket.players.map((player) => {
-			if (player.name == theUser.displayName) {
+		let playersObj = theBracket["players"];
+		let players = [];
+		for (let key in playersObj) {
+			players.push(playersObj[key]);
+		}
+		players.map((player) => {
+			if (player == theUser.displayName) {
+				console.log(joined)
 				joined = true;
+				console.log(joined)
 			}
 		});
 		if (!joined) {
 			$("#addPlayer").on("click", () => {
-				db.ref("brackets/").child(bracketId).child("players").push({ "name": theUser.displayName, "id": theUser.uid });
+				db.ref("brackets/").child(bracketId).child("players").push(theUser.displayName);
 			});
 		}
 		else {
 			$("#addPlayer").remove();
 		}
+		$("#startGame").on("click", () => {
+			startGame(bracketId, players);
+		});
+	});
+
+}
+
+function matchUp(player1, player2) {
+	let p1 = document.createElement("div");
+	let p2 = document.createElement("div");
+	p1.innerHTML = player1;
+	p2.innerHTML = player2;
+	p1.className = "player";
+	p2.className = "player";
+	let match = document.createElement("div");
+	match.className = "match";
+	match.appendChild(p1);
+	match.appendChild(p2);
+	document.getElementById("bracket").appendChild(match);
+	p1.addEventListener("click", () => {
+		console.log("clicked");
+		console.log(p1.getAttribute("class"));
+		console.log(p2.getAttribute("class"));
+		p1.classList.add("winner");
+		p2.classList.remove("winner");
+		console.log(p1.getAttribute("class"));
+		console.log(p2.getAttribute("class"));
+	});
+	p2.addEventListener("click", () => {
+		console.log("clicked");
+		console.log(p1.getAttribute("class"));
+		console.log(p2.getAttribute("class"));
+		p2.classList.add("winner");
+		p1.classList.remove("winner");
+		console.log(p1.getAttribute("class"));
+		console.log(p2.getAttribute("class"));
 	});
 	
 }
@@ -84,7 +128,7 @@ let startApp = (parts) => {
 	}
 	else {
 		if (parts[1] == 'brackets' && parts[2].length > 1) {
-			renderBracket(parts[1]);
+			renderBracket(parts[2]);
 		}
 		else {
 			renderHome();
@@ -92,15 +136,28 @@ let startApp = (parts) => {
 	}
 }
 
-function startGame(bracketId){
-	players = [];
-	db.ref("brackets/").child(bracketId).on("value", (snapshot) => {
-		theBracket = snapshot.val();
-		theBracket.players.map((player) => {
-			players.push(player);
-		});
-	});
+function startGame(bracketId, players) {
+	if(players.length % 2 == 1){
+		players.push("bye");
+	}
+	for (let i = 0; i < players.length; i += 2) {
+		matchUp(players[i], players[i + 1]);
+	}
+}
 
+function getWinners(){
+	let winners = [];
+	let matches = document.getElementsByClassName("match");
+	for(let i = 0; i < matches.length; i++){
+		let match = matches[i];
+		let players = match.getElementsByClassName("player");
+		let winner = players[0].innerHTML;
+		if(players[0].classList.contains("winner")){
+			winner = players[1].innerHTML;
+		}
+		winners.push(winner);
+	}
+	return winners;
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
